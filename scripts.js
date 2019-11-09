885190a0aadd8d49b93b32e8bd0cf8e7a1731056
pellelito <pelle.lofstrand@gmail.com>
2019-11-09 13:28:18 +0100

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -1,23 +1,25 @@
+/*eslint-env browser*/
+/*global $, jQuery, alert*/
 function getRndInteger() {
-  return Math.floor(Math.random()*100  ) + 1;
+    "use strict";
+    return Math.floor(Math.random() * 100) + 1;
 }
 var x = getRndInteger();
-let y =null;
+var y = null;
  //alert(x)
 
 function kollaSvar() {
-
+    "use strict";
 	if (x > document.getElementById("number").value) {
 		alert('För lågt, gissa igen!');
-		 document.getElementById("number").value = '';
-		 ++y;
-} else if (x < document.getElementById("number").value) {
-  		alert('För högt, gissa igen!');
-  		 document.getElementById("number").value = '';
-  		 ++y;
-} else {
-  		alert('Rätt gissat, det tog '  + y + ' gånger. Vill du spela igen?');
-  		location.reload();
-  		// y ='';
-  	}
+        document.getElementById("number").value = '';
+		++y;
+    } else if (x < document.getElementById("number").value) {
+        alert('För högt, gissa igen!');
+        document.getElementById("number").value = '';
+        ++y;
+    } else {
+        alert('Rätt gissat, det tog '  + y + ' gånger. Vill du spela igen?');
+        location.reload();
+    }
 }
